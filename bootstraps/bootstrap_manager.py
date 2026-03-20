1d1ef48e91a62934fb2600566098be0070cc582f
An <annini11234+github@gmail.com>
2026-03-19 22:36:24 -0700

    hot fix disable bootstrap in ci

--- bootstraps/bootstrap_manager.py
+++ bootstraps/bootstrap_manager.py
@@ -2,10 +2,12 @@ from bootstraps.aws import bootstrap_aws
 from bootstraps.credentials import bootstrapping_credentials
 from bootstraps.postgress_check import bootstrap_postgres
 from bootstraps.redis_check import bootstrap_redis
-
+import os
 def bootstrap_manager():
+    if os.getenv('FLASK_ENV') == 'testing':
+        return  # skip bootstrap in CI
     credential = bootstrapping_credentials()
     postgres = bootstrap_postgres()
     redis = bootstrap_redis()
-    # aws = bootstrap_aws()
+    aws = bootstrap_aws()
     print('Bootstrap Pass!')
