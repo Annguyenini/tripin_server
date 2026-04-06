49e70e53b0d33892d298e9ab6dc54dcd1c7b03cc
An <annini11234+github@gmail.com>
2026-04-05 17:35:01 -0700

    role for token

--- migrations/env.py
+++ migrations/env.py
@@ -37,7 +37,7 @@ def run_migrations_online() -> None:
     connectable = engine_from_config(
         configuration,
         prefix="sqlalchemy.",
-        poolclass=pQool.NullPool,
+        poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
---
+++ migrations/versions/15f922fd8223_add_role_to_token.py
@@ -0,0 +1,35 @@
+"""add role to token
+
+Revision ID: 15f922fd8223
+Revises: 7398b77c2ea6
+Create Date: 2026-04-05 17:30:38.815974
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '15f922fd8223'
+down_revision: Union[str, Sequence[str], None] = '7398b77c2ea6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute(f'''
+               ALTER TABLE tripin_auth.tokens ADD COLUMN IF NOT EXISTS role TEXT NOT NULL DEFAULT 'use';
+               ''')
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute(f'''
+            ALTER TABLE tripin_auth.tokens DROP COLUMN IF EXISTS role;
+            ''')
+
+    pass
