ecdbaaba55a13685d6f9115efe95bd3c0850f606
An <annini11234+github@gmail.com>
2026-05-23 22:31:13 -0700

    tweek

--- tests/scripts/create_test_user.py
+++ tests/scripts/create_test_user.py
@@ -4,7 +4,6 @@ import dotenv
 import psycopg2
 from werkzeug.security import generate_password_hash
 
-dotenv.load_dotenv(".env")
 conn = psycopg2.connect(
     host=os.environ.get("DB_HOST"),
     dbname=os.environ.get("DB_NAME"),
@@ -12,11 +11,13 @@ conn = psycopg2.connect(
     password=os.environ.get("DB_PASS"),
     port=os.environ.get("DB_PORT"),
 )
-hashed = generate_password_hash("Testpass11234@")
+username = os.getenv("TEST_USER")
+password = os.getenv("TEST_PASS")
+hashed = generate_password_hash(password)
 cur = conn.cursor()
 cur.execute(
     "INSERT INTO tripin_auth.userdata (email, display_name, user_name, password, created_time, role) VALUES (%s, %s, %s, %s, NOW(), 'user')",
-    ("test@tripping.com", "TestUser", "TestUser", hashed),
+    ("test@tripping.com", username, username, hashed),
 )
 conn.commit()
 cur.execute("SELECT * FROM tripin_auth.userdata WHERE user_name = 'TestUser'")
