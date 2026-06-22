40d177e3d10233c3b318f5bf85aba259f179c4e6
An <annini11234+github@gmail.com>
2026-06-21 18:10:15 -0700

    boottraps

--- bootstraps/aws.py
+++ bootstraps/aws.py
@@ -1,13 +1,21 @@
-import boto3
-from botocore.exceptions import ClientError
 import os
 import signal
 
+import boto3
+from botocore.exceptions import ClientError
+
+
 def bootstrap_aws():
     try:
-        s3 = boto3.client('s3', region_name='us-east-2')
+        s3 = boto3.client(
+            "s3",
+            region_name=os.environ.get("AWS_DEFAULT_REGION"),
+            aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        )
         s3.list_buckets()
         return True
     except Exception as e:
         os.kill(os.getppid(), signal.SIGTERM)  # kill the gunicorn master
+        print("fail to connect to AWS")
         os._exit(1)
--- bootstraps/postgress_check.py
+++ bootstraps/postgress_check.py
@@ -1,21 +1,21 @@
-import psycopg2
 import os
-import dotenv
 import signal
-dotenv.load_dotenv
+
+import dotenv
+import psycopg2
+
+dotenv.load_dotenv()
+
+
 def bootstrap_postgres():
     try:
-        DB_HOST= os.getenv('DB_HOST')
-        DB_PORT= os.getenv('DB_PORT')
-        DB_USER= os.getenv('DB_USER')
-        DB_PASS= os.getenv('DB_PASS')
-        DB_NAME= os.getenv('DB_NAME')
+        DB_HOST = os.getenv("DB_HOST")
+        DB_PORT = os.getenv("DB_PORT")
+        DB_USER = os.getenv("DB_USER")
+        DB_PASS = os.getenv("DB_PASS")
+        DB_NAME = os.getenv("DB_NAME")
         conn = psycopg2.connect(
-            host= DB_HOST,
-            dbname= DB_NAME,
-            user= DB_USER,
-            password= DB_PASS,
-            port= DB_PORT
+            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS, port=DB_PORT
         )
         conn.cursor().execute("SELECT 1")
         return True
