5df2f2c6b81e8648b60fcd58f7fd08410104c728
An <annini11234+github@gmail.com>
2026-06-23 08:42:31 -0700

    fix health

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ class Server:
         self.app.after_request(self.log_request)
 
     def health(self):
-        return 200
+        return {"ok": True}, 200
 
     def testmap(self):
         return render_template("testmap.html")
